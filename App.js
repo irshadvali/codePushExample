407f0b2696d76430bc38effbbb3fb45ad1e487b7
irshadvali <irshad.vali@gmail.com>
2017-11-17 12:27:05 +0530

    initial

---
+++ App.js
@@ -0,0 +1,68 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import codePush from "react-native-code-push";
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+ class App extends Component {
+
+  componentDidMount() {
+    codePush.sync({
+      updateDialog: true,
+      installMode: codePush.InstallMode.IMMEDIATE
+    });
+    console.disableYellowBox = true;
+  }
+
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Learn to CodePush!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+export default App;
---
+++ android/app/src/main/java/com/codepushexample/MainApplication.java
@@ -0,0 +1,55 @@
+package com.codepushexample;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+  private final String staging_React_Native_Key = "WLL2FLbQAt2_cC4_BDvYLDtE05Fv4d40d29b-d7a5-4ccd-ab5a-d16cda9561e6";
+
+  private final String production_React_Native_Key = "z4gK760K2V3PDhkyrNTfyXs-GrtR4d40d29b-d7a5-4ccd-ab5a-d16cda9561e6";
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+        @Override
+        protected String getJSBundleFile() {
+        return CodePush.getJSBundleFile();
+        }
+    
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new CodePush(staging_React_Native_Key, MainApplication.this, BuildConfig.DEBUG)
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
